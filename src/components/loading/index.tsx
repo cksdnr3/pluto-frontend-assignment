4c284214efd62441839538009a048e94ea8c60da
cksdnr3 <cksdnr3333@gmail.com>
2022-05-01 23:33:55 +0900

    implements loading component

--- src/components/loading/index.tsx
+++ src/components/loading/index.tsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
 function Loading() {
   return (
     <Wrapper>
-      <Icon>📚 ...</Icon>
+      <Icon>📚</Icon>
     </Wrapper>
   );
 }
@@ -12,13 +12,29 @@ function Loading() {
 const Wrapper = styled.div`
   display: flex;
   align-items: center;
-  justify-content: center;
-  opacity: 0.7;
+  margin: auto;
+  width: 150px;
   height: 100%;
   font-size: 3rem;
   text-align: center;
+  opacity: 0.7;
+  overflow: hidden;
 `;
 
-const Icon = styled.span``;
+const Icon = styled.span`
+  @keyframes loop {
+    from {
+      transform: translate(120px);
+    }
+
+    to {
+      transform: translate(-100px);
+    }
+  }
+
+  animation-name: loop;
+  animation-iteration-count: infinite;
+  animation-duration: 0.65s;
+`;
 
 export default Loading;
